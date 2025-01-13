73024c2333e11691701a9473ec8cb4f36d12cbaa
Dora Zhao <dorazhao99@gmail.com>
2025-01-13 00:23:40 +0000

    Add user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,7 +40,7 @@ export const createUser = async (req, res, next) => {
                 githubID: data.githubID,
                 userName: screenName, 
                 token: token,
-                modules: [],
+                modules: ['Owpgm2DXhp0a0dnM1gZa'],
                 checked: {}
             }
             try {
