026ddcadb8120d494ed94360b5af345f5d89981e
Dora Zhao <dorazhao@DNa80d5dc.SUNet>
2025-02-03 23:08:34 -0800

    Increase payload size

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ var accessLogStream = fs.createWriteStream(path.join(path.dirname(__filename), '
 // setup the logger
 app.use(morgan('short', { stream: accessLogStream, skip: function (req, res) { return res.statusCode < 400 } }))
 app.use(cors(corsOptions));
-app.use(express.json());
-
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 //routes
 app.use('/api', moduleRoute);
 
