cd385ab0b2e57d8b223a5882131a80e46e87fbc6
Dora Zhao <dorazhao@DNa80dd0a.SUNet>
2025-02-08 18:20:55 -0800

    Store message and time

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -4,12 +4,42 @@ import { db } from '../firebase.js';
 export const storeMessage = async (req, res, next) => {
     const data = req.body
     if (data.uid && data.messageId) {
+        const savedModules = []
+
+        if (Array.isArray(data.modules)) {
+            data.modules.forEach(module => {
+                console.log(module, data.modules);
+                if (module !== null) {
+                    if (module?.name) {
+                        savedModules.push(module.name)
+                    } else {
+                        savedModules.push(module)
+                    }
+                }
+            })
+        } else {
+            Object.keys(data.modules).forEach(m => {
+                let module = data.modules[m]
+                console.log(m, module, data.modules)
+                if (module !== null) {
+                    if (module?.name) {
+                        savedModules.push(module.name)
+                    } else {
+                        savedModules.push(module)
+                    }
+                }
+            })
+        }
+
+        const message = data.message ? data.message : ""
         const d = {
+            message: message,
             conversationId: data.conversationId,
             messageId: data.messageId,
             uid: data.uid, 
-            modules: data.modules,
-            provider: data.provider
+            modules: savedModules,
+            provider: data.provider,
+            time: Date.now()
         }
 
         try {
